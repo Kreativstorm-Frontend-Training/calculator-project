8b68c6f7c9813acec207f92fd73dae048b663ec0
Fredy Aristizabal <freddyarh13@gmail.com>
2023-10-02 19:54:10 +1100

    Interface calculator

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 console.log("test");
+
+const h1 = document.getElementsByTagName('h1');
+let arr = [];
+const handleButton = (value) => {
+    if( typeof value === 'number' ){
+        arr.push(value);
+        const num = arr.join("");
+        const input = document.getElementsByTagName('input')[0].defaultValue = num;
+    }
+}
