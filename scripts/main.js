b9dbb12c6e46a052082463f922b3b17abc3f6b73
tranduynhat0209 <nhatducmo@gmail.com>
2023-10-03 14:42:42 +0700

    fix the layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 console.log("test");
 
-const h1 = document.getElementsByTagName('h1');
+const h1 = document.getElementsByTagName("h1");
 let arr = [];
-const handleButton = (value) => {
-    if( typeof value === 'number' ){
-        arr.push(value);
-        const num = arr.join("");
-        const input = document.getElementsByTagName('input')[0].defaultValue = num;
-    }
+function handleButton(value) {
+  if (typeof value === "number") {
+    handleDigit(value);
+  }
+}
+
+function handleDigit(digit) {
+  arr.push(digit);
+  const num = arr.join("");
+  const input = (document.getElementsByTagName("input")[0].defaultValue = num);
 }
