5749b6bd03e3065e1b49047cc317e20fef229476
Moses Ebenardor-Amiedor <atmdboss@yahoo.com>
2023-10-07 21:30:27 +0200

    code cleanup; added README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Calculator
+Simple calculator capable of performing basic operations, with added keyboard support
+
+## Demo
+[Check it out](https://kreativstorm-frontend-training.github.io/calculator-project/)
+
+## Screenshots
+![App Screenshot](./images/screenshot.png)
+
+## Technologies Used
+HTML, CSS & JavaScript
+
+## Authors
+- Imaan Cooper
+- Only Only
+- Freddy Aristizabal
+- Moses Ebenardor
+- Arthur Kamarouski
--- scripts/main.js
+++ scripts/main.js
@@ -1,271 +1,272 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // SCREEN TARGETS
-  const oldValueDisplay = document.getElementById("oldValueDisplay");
-  const operatorDisplay = document.getElementById("operatorDisplay");
-  const mainValueDisplay = document.getElementById("mainValueDisplay");
-  // BUTTON TARGETS
-  const operatorButtons = document.querySelectorAll("button.button--operator");
-  const numberButtons = document.querySelectorAll("button.button--number");
-  const dotButton = document.querySelector("button.button--dot");
-  const backspaceButton = document.querySelector("button.button--delete");
-  const utilityButtons = document.querySelectorAll("button.button--utility");
+// SCREEN TARGETS
+const topValueDisplay = document.getElementById("oldValueDisplay");
+const operatorDisplay = document.getElementById("operatorDisplay");
+const mainValueDisplay = document.getElementById("mainValueDisplay");
+// BUTTON TARGETS
+const operatorButtons = document.querySelectorAll("button.button--operator");
+const numberButtons = document.querySelectorAll("button.button--number");
+const dotButton = document.querySelector("button.button--dot");
+const backspaceButton = document.querySelector("button.button--delete");
+const utilityButtons = document.querySelectorAll("button.button--utility");
 
-  let currentValue = "";
-  const MAX_DISPLAY_LENGTH = 12;
+let currentValue = "";
+const MAX_DISPLAY_LENGTH = 12;
 
-  // EVENT LISTENERS
-  operatorButtons.forEach((element) => {
-    element.addEventListener("click", (event) =>
-      handleOperator(event.target.textContent)
-    );
-  });
-  numberButtons.forEach((element) => {
-    element.addEventListener("click", (event) =>
-      handleNumber(event.target.textContent)
-    );
-  });
-  utilityButtons.forEach((element) => {
-    element.addEventListener("click", (event) =>
-      handleUtility(event.target.textContent)
-    );
-  });
-  dotButton.addEventListener("click", handleDot);
-  backspaceButton.addEventListener("click", handleBackspace);
-  document.addEventListener("keydown", (event) => handleKeyboard(event.key));
+// EVENT LISTENERS
+operatorButtons.forEach((element) => {
+    element.addEventListener("click", (event) => handleOperator(event.target.textContent));
+});
+numberButtons.forEach((element) => {
+    element.addEventListener("click", (event) => handleNumber(event.target.textContent));
+});
+utilityButtons.forEach((element) => {
+    element.addEventListener("click", (event) => handleUtility(event.target.textContent));
+});
+dotButton.addEventListener("click", handleDot);
+backspaceButton.addEventListener("click", handleBackspace);
+document.addEventListener("keydown", (event) => handleKeyboard(event.key));
 
-  function handleKeyboard(keyPressed) {
+function handleKeyboard(keyPressed) {
     switch (keyPressed) {
-      case "0":
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-        handleNumber(keyPressed);
-        break;
-      case "=":
-      case "Enter":
-        handleOperator("=");
-        break;
-      case "+":
-        handleOperator(keyPressed);
-        break;
-      case "-":
-        handleOperator("-");
-        break;
-      case "*":
-        handleOperator("x");
-        break;
-      case "/":
-        handleOperator("÷");
-        break;
-      case ".":
-        handleDot();
-        break;
-      case "Escape":
-        handleUtility("AC");
-        break;
-      case "Backspace":
-        handleBackspace();
-        break;
-      case "%":
-        handleUtility("%");
-        break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            handleNumber(keyPressed);
+            break;
+        case "=":
+        case "Enter":
+            handleOperator("=");
+            break;
+        case "+":
+            handleOperator(keyPressed);
+            break;
+        case "-":
+            handleOperator("-");
+            break;
+        case "*":
+        case "x":
+        case "X":
+            handleOperator("x");
+            break;
+        case "/":
+            handleOperator("÷");
+            break;
+        case ".":
+            handleDot();
+            break;
+        case "Escape":
+            handleUtility("AC");
+            break;
+        case "Backspace":
+            handleBackspace();
+            break;
+        case "%":
+            handleUtility("%");
+            break;
 
-      default:
-        break;
+        default:
+            break;
     }
-  }
+}
 
-  function handleBackspace() {
+function handleBackspace() {
     if (mainValueDisplay.textContent.length) {
-      if (mainValueDisplay.textContent.includes("e")) currentValue = "0";
-      else currentValue = mainValueDisplay.textContent.slice(0, -1);
-      mainValueDisplay.textContent = currentValue;
+        if (isOutOfBoundsNumber(mainValueDisplay.textContent)) currentValue = "0";
+        else currentValue = mainValueDisplay.textContent.slice(0, -1);
+        mainValueDisplay.textContent = currentValue;
     }
-  }
+}
 
-  function handleOperator(operatorClicked) {
+function handleOperator(operatorClicked) {
     if (mainValueDisplay.textContent.endsWith(".")) return;
 
     if (operatorClicked === "=") {
-      handleCalculation();
-      oldValueDisplay.textContent = "";
-      operatorDisplay.textContent = "";
-      return;
-    } else {
-      const shouldCalculateFirst =
-        Boolean(oldValueDisplay.textContent) &&
-        Boolean(operatorDisplay.textContent) &&
-        Boolean(mainValueDisplay.textContent);
-
-      if (shouldCalculateFirst) {
         handleCalculation();
-        // move to top
-        oldValueDisplay.textContent = currentValue; //new current value from calculation
-        operatorDisplay.textContent = operatorClicked;
-        mainValueDisplay.textContent = "";
-        currentValue = "";
-      } else if (oldValueDisplay.textContent && operatorDisplay.textContent) {
-        //all we want here is to change the operator
-        operatorDisplay.textContent = operatorClicked;
-      } else if (currentValue.length === 0) {
+        topValueDisplay.textContent = "";
+        operatorDisplay.textContent = "";
         return;
-      } else {
-        oldValueDisplay.textContent = currentValue;
-        operatorDisplay.textContent = operatorClicked;
-        mainValueDisplay.textContent = "";
-        currentValue = "";
-      }
-    }
-  }
+    } else {
+        const shouldCalculateFirst = canCalculate();
 
-  function handleNumber(numberClicked) {
+        if (shouldCalculateFirst) {
+            handleCalculation();
+            // move to top
+            topValueDisplay.textContent = currentValue; //new current value from calculation
+            operatorDisplay.textContent = operatorClicked;
+            mainValueDisplay.textContent = "";
+            currentValue = "";
+        } else if (topValueDisplay.textContent && operatorDisplay.textContent) {
+            //all we want here is to change the operator
+            operatorDisplay.textContent = operatorClicked;
+        } else if (currentValue.length === 0) {
+        return;
+        } else {
+            topValueDisplay.textContent = currentValue;
+            operatorDisplay.textContent = operatorClicked;
+            mainValueDisplay.textContent = "";
+            currentValue = "";
+        }
+    }
+}
+
+function handleNumber(numberClicked) {
     if (currentValue.length >= MAX_DISPLAY_LENGTH) return;
 
     if (currentValue.length === 0 || currentValue === "0") {
-      currentValue = numberClicked;
+        currentValue = numberClicked;
     } else {
-      currentValue = `${currentValue}${numberClicked}`;
+        currentValue = `${currentValue}${numberClicked}`;
     }
 
     mainValueDisplay.textContent = currentValue;
-  }
+}
 
-  function handleUtility(utilityValue) {
+function handleUtility(utilityValue) {
     switch (utilityValue) {
-      case "AC":
-        handleReset();
-        break;
-      case "+/-":
-        handleTogglePositivity();
-        break;
-      case "%":
-        handleTogglePercentage();
-        break;
+        case "AC":
+            handleReset();
+            break;
+        case "+/-":
+            handleTogglePositivity();
+            break;
+        case "%":
+            handleTogglePercentage();
+            break;
 
-      default:
-        break;
+        default:
+            break;
     }
-  }
+}
 
-  function handleReset() {
-    oldValueDisplay.textContent = "";
+function handleReset() {
+    topValueDisplay.textContent = "";
     mainValueDisplay.textContent = "";
     operatorDisplay.textContent = "";
     currentValue = "";
-  }
+}
 
-  function handleTogglePositivity() {
-    if (currentValue.length >= MAX_DISPLAY_LENGTH || currentValue === "0")
-      return;
+function handleTogglePositivity() {
+    if (currentValue.length >= MAX_DISPLAY_LENGTH || currentValue === "0") return;
 
     if (mainValueDisplay.textContent) {
-      if (currentValue.includes("-")) {
-        currentValue = currentValue.split("-")[1];
-      } else {
-        currentValue = `-${currentValue}`;
-      }
+        if (currentValue.includes("-")) {
+            currentValue = currentValue.split("-")[1];
+        } else {
+            currentValue = `-${currentValue}`;
+        }
 
-      mainValueDisplay.textContent = currentValue;
+        mainValueDisplay.textContent = currentValue;
     }
-  }
+}
 
-  function handleTogglePercentage() {
+function handleTogglePercentage() {
     if (currentValue === "0") return;
 
     if (mainValueDisplay.textContent) {
-      currentValue = (sanitizeNumber(currentValue) / 100).toString();
-      mainValueDisplay.textContent = currentValue;
+        currentValue = (sanitizeNumber(currentValue) / 100).toString();
+        mainValueDisplay.textContent = currentValue;
     }
-  }
+}
 
-  function handleDot() {
-    if (currentValue.length >= MAX_DISPLAY_LENGTH || currentValue.includes("."))
-      return;
+function handleDot() {
+    if (currentValue.length >= MAX_DISPLAY_LENGTH || currentValue.includes(".")) return;
 
     if (currentValue.length === 0) {
-      currentValue = "0.";
+        currentValue = "0.";
     } else {
-      currentValue = `${currentValue}.`;
+        currentValue = `${currentValue}.`;
     }
 
     mainValueDisplay.textContent = currentValue;
-  }
+}
 
-  function handleCalculation() {
-    if (
-      oldValueDisplay.textContent &&
-      mainValueDisplay.textContent &&
-      operatorDisplay.textContent
-    ) {
-      if (mainValueDisplay.textContent.endsWith(".")) return;
+function handleCalculation() {
+    if (canCalculate()) {
+        if (mainValueDisplay.textContent.endsWith(".")) return;
 
-      const leftHandSide = sanitizeNumber(oldValueDisplay.textContent);
-      const rightHandSide = sanitizeNumber(mainValueDisplay.textContent);
+        const leftHandSide = sanitizeNumber(topValueDisplay.textContent);
+        const rightHandSide = sanitizeNumber(mainValueDisplay.textContent);
 
-      switch (operatorDisplay.textContent) {
-        case "÷":
-          currentValue =
-            rightHandSide === 0
-              ? (() => {
-                  alert("You know you can't do that silly");
-                  return "0";
-                })()
-              : sanitizeNumber(leftHandSide / rightHandSide).toString();
-          break;
-        case "x":
-          currentValue = sanitizeNumber(
-            leftHandSide * rightHandSide
-          ).toString();
-          break;
-        case "–":
-          currentValue = sanitizeNumber(
-            leftHandSide - rightHandSide
-          ).toString();
-          break;
+        switch (operatorDisplay.textContent) {
+            case "÷":
+                currentValue = rightHandSide === 0
+                    ? (() => {
+                        alert("You know you can't do that silly");
+                        return "0";
+                    })()
+                    : sanitizeNumber(leftHandSide / rightHandSide).toString();
+                break;
+            case "x":
+                currentValue = sanitizeNumber(leftHandSide * rightHandSide).toString();
+                break;
+            case "–":
+                currentValue = sanitizeNumber(leftHandSide - rightHandSide).toString();
+                break;
 
-        default:
-          currentValue = sanitizeNumber(
-            leftHandSide + rightHandSide
-          ).toString();
-          break;
-      }
+            default:
+                currentValue = sanitizeNumber(leftHandSide + rightHandSide).toString();
+                break;
+        }
 
-      mainValueDisplay.textContent = currentValue;
+        mainValueDisplay.textContent = currentValue;
     }
-  }
+}
 
-  function sanitizeNumber(numberToSanitize) {
+function sanitizeNumber(numberToSanitize) {
+    if(typeof numberToCheck !== "number" && typeof numberToCheck !== "string")  return 0;
+
     if (typeof numberToSanitize === "number") {
-      // handle too large or too small numbers
-      if (numberToSanitize.toString().length >= MAX_DISPLAY_LENGTH) {
-        numberToSanitize = numberToSanitize.toExponential(2);
-      } else {
-        numberToSanitize = numberToSanitize.toString();
-      }
+        // handle too large or too small numbers
+        if (numberToSanitize.toString().length >= MAX_DISPLAY_LENGTH) {
+            numberToSanitize = numberToSanitize.toExponential(2);
+        } else {
+            numberToSanitize = numberToSanitize.toString();
+        }
     }
 
     const isFloat = numberToSanitize.includes(".");
     if (isFloat) {
-      if (numberToSanitize.includes("-") || numberToSanitize.includes("+")) {
-        // is out of bounds number
-        return numberToSanitize;
-      } else {
-        //is normal float
-        const decimalPlaceLength = numberToSanitize.split(".")[1].length;
-        if (decimalPlaceLength > 1) {
-          return Number(parseFloat(numberToSanitize).toFixed(2));
+        if (isOutOfBoundsNumber(numberToSanitize)) {
+            return numberToSanitize;
         } else {
-          return parseFloat(numberToSanitize);
+            //is normal float
+            const decimalPlaceLength = numberToSanitize.split(".")[1].length;
+            if (decimalPlaceLength > 1) {
+                return Number(parseFloat(numberToSanitize).toFixed(2));
+            } else {
+                return parseFloat(numberToSanitize);
+            }
         }
-      }
     } else {
-      return parseInt(numberToSanitize);
+        return parseInt(numberToSanitize);
     }
-  }
-});
+}
+
+function isOutOfBoundsNumber(numberToCheck) {
+    if(typeof numberToCheck !== "number" && typeof numberToCheck !== "string")  return;
+
+    if(typeof numberToCheck === "number"){
+        numberToCheck = numberToCheck.toString();
+    }
+
+    // if number is exceptionally long or has been sanitized and now looks like exponent
+    if(numberToCheck.length >= MAX_DISPLAY_LENGTH || 
+        (numberToCheck.includes(".") && numberToCheck.includes("e"))) {
+        return true;
+    } 
+
+    return false;
+}
+
+function canCalculate() {
+    return Boolean(topValueDisplay.textContent) &&
+    Boolean(operatorDisplay.textContent) &&
+    Boolean(mainValueDisplay.textContent);
+}
